06b7deb91cc134da61d9ea87fdbfc93809e81164
bscott101 <brendan-scott@live.com.au>
2025-09-10 21:22:49 +1000

    :fire: We got rotation :fire:

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import pygame
 from pygame.base import init
-
+from player import Player
 from constants import *
+
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-   
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1_000
+
 
 if __name__ == "__main__":
     main()

9052b622a56d94238c4ad62f1cb7375d74980898
bscott101 <brendan-scott@live.com.au>
2025-09-11 21:55:28 +1000

    group added for player

--- main.py
+++ main.py
@@ -7,19 +7,27 @@ from constants import *
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
-
         screen.fill("black")
-        player.draw(screen)
+        updateable.update(dt)
+
+        for draws in drawable:
+            draws.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1_000
